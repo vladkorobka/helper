2463b01cd6eb55d2565eafea8ea03b9dea6d0280
Vladyslav Korobka <vladseipa@gmail.com>
2026-04-27 22:48:37 +0300

    fixed: change blur effect on Navigation

--- components/layout/Navigation.jsx
+++ components/layout/Navigation.jsx
@@ -25,7 +25,7 @@ export default function Navigation() {
   };
 
   return (
-    <nav className="glass-nav sticky top-0 z-20">
+    <nav className="glass-nav sticky top-0 z-20 backdrop-blur-xs">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-14">
           {/* Logo */}
